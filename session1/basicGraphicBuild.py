5614aa9e089cbde8a6137f76d8322dbaefaea9b7
joowon <saven7788@gmail.com>
2017-04-20 22:07:34 +0900

    위치바꾸

--- session1/basicGraphicBuild.py
+++ session1/basicGraphicBuild.py
@@ -24,3 +24,15 @@ print("node3: node3")
 
  # sess.run( node1,node2) : [3.0,4.0]
  # sess.run(node3) : 7.0
+
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b
+
+#placeholder은 프로그램 실행 중에 데이터를 변경할 수 있게 해주는 것이며 이것은 심벌릭 변수를 선언할 때 쓰인다.
+
+print(sess.run(adder_node , feed_dict = { a:3 , b:4.5 })) #7.5
+# feed_dict란 placeholder에 있는 것 가져오기
+
+print(sess.run(adder_node , feed_dict = {a: [1,3], b:[2,4]})) #[3,7]
